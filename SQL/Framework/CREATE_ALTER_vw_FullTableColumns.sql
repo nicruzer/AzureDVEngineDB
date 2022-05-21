792493c1c10326a9ff01f699e08d924403f3d8e1
nicruzer <60755953+nicruzer@users.noreply.github.com>
2022-05-20 19:46:32 -0500

    Stage2 View Creation

--- SQL/Framework/CREATE_ALTER_vw_FullTableColumns.sql
+++ SQL/Framework/CREATE_ALTER_vw_FullTableColumns.sql
@@ -2,6 +2,8 @@ CREATE OR ALTER VIEW dbo.vw_FullTableColumns
 AS
     SELECT vctv.Id AS TableVersionId, vctv.TableId,
         t.TableSchema, t.TableName, t.Abbreviation AS EntityAbbreviation,
+        CONCAT_WS('.',QUOTENAME(t.TableSchema),QUOTENAME(CONCAT_WS('_',t.Abbreviation, t.TableName))) AS FQTableName,
+        QUOTENAME(CONCAT_WS('_',t.Abbreviation,t.TableName)) AS FullTableName,
         t.RecordSourceId, dbo.fn_GetRecordSource(t.RecordSourceId) AS RSRC,
         c.Id AS ColumnId, c.ColumnName, c.DataType, c.CharacterMaxLength,
         c.DateNumPrecision, c.IsNullable, c.OrdinalPosition, c.Abbreviation AS AttributeAbbreviation
